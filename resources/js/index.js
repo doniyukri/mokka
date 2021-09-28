e687a9053e8c46bbd5682621853e8167c3e6feaf
unknown <doni.yukri@gmail.com>
2021-09-28 22:34:43 +0700

    add jquery and its addons for nav

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,20 @@
 "use strict";
 
-// change year on copyright
-const year = new Date();
-document.querySelector(".js--footer__copyright").innerHTML = year.getFullYear();
+$(document).ready(function () {
+  // change year on copyright
+  const year = new Date();
+  document.querySelector(".js--footer__copyright").innerHTML =
+    year.getFullYear();
+
+  //Show floating nav
+  $(".js--products").waypoint(
+    function (direction) {
+      if (direction === "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    { offset: "40px" }
+  );
+});
