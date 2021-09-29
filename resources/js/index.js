ed5eedfc5f6ecd6ba6dba90723dd9f6241ee0b7f
unknown <doni.yukri@gmail.com>
2021-09-29 16:19:59 +0700

    add animation and fix nav

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,14 @@
 "use strict";
 
-$(document).ready(function () {
+$(document).ready(() => {
   // change year on copyright
   const year = new Date();
   document.querySelector(".js--footer__copyright").innerHTML =
     year.getFullYear();
 
-  //Show floating nav
+  // show floating nav whenever leave header
   $(".js--products").waypoint(
-    function (direction) {
+    (direction) => {
       if (direction === "down") {
         $("nav").addClass("sticky");
       } else {
@@ -17,4 +17,26 @@ $(document).ready(function () {
     },
     { offset: "40px" }
   );
+
+  // ------------ element animation
+  const animate = (cssQuery, animationType, offset) => {
+    $(cssQuery).waypoint(
+      (direction) => {
+        $(cssQuery).addClass(`animate__animated animate__${animationType}`);
+      },
+      { offset: `${offset}%` }
+    );
+  };
+
+  // animate coffee detail
+  animate(".js__wp--1", "fadeIn", "60");
+
+  //animate shop picture
+  animate(".js__wp--2", "fadeInLeft", "50");
+
+  // animate roastery picture
+  animate(".js__wp--3", "fadeInRight", "50");
+
+  // animate first suvscribtion plan
+  animate(".js__wp--4", "pulse", "25");
 });
